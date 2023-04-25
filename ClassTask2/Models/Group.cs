aa8a05a3aba932c7d7a72677b0e3edb53c10867a
LemanMammad <101627982+LemanMammadly@users.noreply.github.com>
2023-04-26 00:28:55 +0400

    classtask

---
+++ ClassTask2/Models/Group.cs
@@ -0,0 +1,37 @@
+﻿public class Group
+{
+    public string No { get; set; }
+    public int Limit { get; set; }
+    public Student[] Students { get; private set; }
+
+
+    public Group(string no, int limit, Student[] students)
+    {
+        No = no;
+        Limit = limit;
+        Students = students;
+    }
+
+    public string AddStudent(Student student)
+    {
+        if(Students.Length<Limit)
+        {
+            return "Yer mövcuddur.";
+        }
+        else
+        {
+            return "Yer mövcud deyil.";
+        }
+    }
+
+    public void GetStudents()
+    {
+        foreach (Student student in Students)
+        {
+            Console.WriteLine($"{student.Name} {student.Surname}");
+        }
+    }
+}
+
+
+
---
+++ ClassTask2/Program.cs
@@ -0,0 +1,19 @@
+﻿Student stu1 = new("Laman", "Mammadli");
+Student stu2 = new("Lam", "Mammad");
+Student stu3 = new("Lmn", "Mdli");
+Student stu4 = new("L", "M");
+Student stu5 = new("leman", "Mammad");
+
+Student[] arr1 = new Student[4];
+arr1[0] = stu1;
+arr1[1] = stu2;
+arr1[2] = stu3;
+arr1[3] = stu4;
+
+Group group1 = new("P137", 4, arr1);
+
+
+Console.WriteLine(group1.AddStudent(stu5));
+group1.GetStudents();
+
+
